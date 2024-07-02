f139f9b3e51ed20424059302b2af28dbbd34ae3e
YuTen <st99640@gmail.com>
2024-07-02 12:23:20 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,8 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createPinia } from 'pinia'
 
-axios.defaults.baseURL = 'http://13.114.100.49:8080'
-// axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 const pinia = createPinia()
 
